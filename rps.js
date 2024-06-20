217b213a8237cca0fb15da78e234c50d9a978822
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-06-20 19:49:49 +0800

    Write script test #1

--- rps.js
+++ rps.js
@@ -1,1 +1,22 @@
 console.log("hello world!");
+console.clear();
+
+// Random number between 1-3
+// based on the number get rock, paper, or scissors
+function getComputerChoice() {
+    // 1. RANDOMIZE rand (1-3)
+    let rand = Math.floor((Math.random() * 3) + 1);
+    // 2. CHOOSE decision based on random value
+    switch(rand) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            console.error("Something went wrong!");
+    }
+}
+
+let computerChoice = getComputerChoice();
